6517dc6c79d7360b9e20471a18194e637415afcf
4uvp <Bobstripping@gmail.com>
2026-01-12 21:00:44 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,12 @@
-
-print([[
+--[[
   _    _                         ____       _        
  | |  | |                       |  _ \     | |       
  | |__| | __ ___   _____   ___  | |_) | ___| |_ __ _ 
  |  __  |/ _` \ \ / / _ \ / __| |  _ < / _ \ __/ _` |
  | |  | | (_| |\ V / (_) | (__  | |_) |  __/ || (_| |
  |_|  |_|\__,_| \_/ \___/ \___| |____/ \___|\__\__,_|
-                --- Havoc Beta Loader
-]])
-
+                --- Havoc Beta Loader 
+]]
 
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/20c771b78c8a42b504b7d6feb4a669ed5fc274b5290c5b469929d6d09011098b/download"))()
+
